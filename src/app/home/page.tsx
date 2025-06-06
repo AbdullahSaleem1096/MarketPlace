9ff8c376968c4edabe317b30db11bb1a51b4f188
AbdullahSaleem1096 <abdullahsaleem1096@gmail.com>
2025-06-06 16:01:01 +0500

    adding the logout button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,24 @@
 "use client"
+import axios from "axios";
+import {useRouter} from "next/navigation";
+
 
 export default function home(){
+    const router = useRouter();
+    const logout = async () => {
+        try{
+            await axios.get('/api/users/logout');
+            router.push("/login");
+        }
+        catch(error:any){
+            console.log(error.message);
+        }
+    }
     return (
-        <h2>home</h2>
+        <div>
+            <h2>home</h2>
+            <hr />
+            <button onClick={logout}>Logout</button>
+        </div>
     )
 }
