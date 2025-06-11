c70df18346ab461a8b640198e90a67924e202c23
AbdullahSaleem1096 <abdullahsaleem1096@gmail.com>
2025-06-11 16:29:26 +0500

    adding the fetching product functionality in the home page

---
+++ src/app/api/products/get/route.js
@@ -0,0 +1,23 @@
+import { connect } from "@/dbConfig/dbConfig";
+import Product from "@/models/productModels";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connect();
+        
+        const products = await Product.find({ isAvailable: true })
+            .sort({ createdAt: -1 });
+
+        return NextResponse.json({
+            success: true,
+            products
+        });
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        );
+    }
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,63 @@
 "use client"
 import axios from "axios";
-import {useRouter} from "next/navigation";
+import { useState, useEffect } from 'react'
+import { useRouter } from "next/navigation";
 
+interface Product {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    image: string;
+    quantity: number;
+}
 
-export default function home(){
+export default function Home() {
+    const [products, setProducts] = useState<Product[]>([]);
     const router = useRouter();
+
     const logout = async () => {
-        try{
+        try {
             await axios.get('/api/users/logout');
             router.push("/login");
         }
-        catch(error:any){
+        catch (error: any) {
             console.log(error.message);
         }
     }
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('/api/products/get');
+                if (response.data.success) {
+                    setProducts(response.data.products);
+                } else {
+                    console.log("Error in fetching the data:", response.data.error);
+                }
+            }
+            catch (error: any) {
+                console.log("Error fetching products:", error.message);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
     return (
         <div>
-            <h2>home</h2>
+            <h2>Home</h2>
+            <hr />
+            <div>
+                {products.map((product) => (
+                    <div key={product._id}>
+                        <img src={product.image} alt={product.name} />
+                        <h2>{product.name}</h2>
+                        <p>${product.price}</p>
+                    </div>
+                ))}
+            </div>
             <hr />
             <button onClick={logout}>Logout</button>
         </div>
